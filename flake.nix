2d0595be05c3b53b12d97f0c6350ddeb1b2dfd1d
arch <arch@local>
2023-05-13 19:05:14 +0200

    add comment

--- flake.nix
+++ flake.nix
@@ -21,8 +21,8 @@
         version = "0.0.1";
         src = fetchgit {
           url = "https://github.com/michael-mueller-git/jt-flow.git";
-          rev = "e062c0828624fb32048b982463d23076dde691a8";
-          sha256 = "sha256-BLj2yJ/NKg37bt1YxX5iUhIuayvR9p7+BzfY8oJhoMg=";
+          rev = "84f899066b95a0fd4dd0aab17067eca38c009305";
+          sha256 = "sha256-jd08B0dewhbtQjkJwRfy+xKLuhRyqQNPBHfcXKtCT24=";
           fetchSubmodules = true;
         };
         nativeBuildInputs = [
@@ -46,30 +46,28 @@
           pkg-config
         ];
         configurePhase = ''
-          # sed -i 's/nvcuvid.h/dynlink_nvcuvid.h/g' FlowLib/cuda/nv12_to_rgb.cu
-          # sed -i 's/nvcuvid.h/dynlink_nvcuvid.h/g' FlowLib/cuda/NvDecoder.h
-          # sed -i 's/nvcuvid.h/dynlink_nvcuvid.h/g' FlowLib/cuda/ReadFlow.cu
           echo "include_directories(${pkgs.nv-codec-headers-11}/include/ffnvcodec)" >> FlowLib/CMakeLists.txt
         '';
-          buildPhase = "${pkgs.cmake}/bin/cmake FlowLib -DCUDA_CUDART_LIBRARY=${pkgs.cudaPackages.cudatoolkit}/lib -DOpenCL_INCLUDE_DIR=${pkgs.opencl-headers}/include -DOpenCL_LIBRARY=${pkgs.clang-ocl}/lib -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_STANDARD=17 -DOpenCV_DIR=${pkgs.opencv}/lib/cmake/opencv4 -DPython3_NumPy_INCLUDE_DIRS=${pkgs.python310Packages.numpy} -DCMAKE_CUDA_ARCHITECTURES=75 && make -j $NIX_BUILD_CORES";
-          installPhase = ''
-            mkdir -p $out/bin
-            mv FlowLibUtil $out/bin
-          '';
-        }
-        );
-      in
-      rec {
-        formatter = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-        defaultApp = flake-utils.lib.mkApp {
-          drv = defaultPackage;
-        };
-        defaultPackage = jt-flow;
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            jt-flow
-            pkgs.opencv
-              ];
+        buildPhase = "${pkgs.cmake}/bin/cmake FlowLib -DNIX=ON -DCUDA_CUDART_LIBRARY=${pkgs.cudaPackages.cudatoolkit}/lib -DOpenCL_INCLUDE_DIR=${pkgs.opencl-headers}/include -DOpenCL_LIBRARY=${pkgs.clang-ocl}/lib -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_STANDARD=17 -DOpenCV_DIR=${pkgs.opencv}/lib/cmake/opencv4 -DPython3_NumPy_INCLUDE_DIRS=${pkgs.python310Packages.numpy} -DCMAKE_CUDA_ARCHITECTURES=75 && make -j $NIX_BUILD_CORES";
+        installPhase = ''
+          mkdir -p $out/bin
+          # mv FlowLibUtil $out/bin
+          echo "TODO copy all binaries when cuda problem is fixed"
+        '';
+      }
+      );
+    in
+    rec {
+      formatter = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
+      defaultPackage = jt-flow;
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          jt-flow
+          pkgs.opencv
+        ];
       };
     }
   );
